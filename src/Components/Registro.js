f86edd4c8f35f0bfba95a735e5f4aa460cb0da1c
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-07-12 20:51:28 -0300

    Update Registro.js

--- src/Components/Registro.js
+++ src/Components/Registro.js
@@ -1,7 +1,48 @@
+import { options } from "yargs";
+
 export const Register = () => {
     const registerApi = async () => {
         const urlBase = "https://redis-auth.herokuapp.com";
         const url = `${urlBase}/auth/Register`;
-        const response = await fetch(url);
+        const response = await fetch(url, options);
+        if (response.ok) {
+            alert(`Usuario ${data.id} registrado`);
+        } else {
+            alert(`No se pudo crear usuario`);
+        }
     };
+    handleChange = (event) =>{
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({
+            [name]: value
+        })
+    }
+    return(
+        <div className="App">
+        <form>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            placeholder="Name"
+          />
+          <input
+            type="text"
+            onChange={this.handleChange}
+            placeholder="eMail"
+          />
+          <input
+            type="text"
+            onChange={this.handleChange}
+            placeholder="Password"
+          />
+          <input
+            type="text"
+            onChange={this.handleChange}
+            placeholder="Confirm Password"
+          />
+          <button /*onClick={() => this.handleClick(tarea.id)}*/>Register</button>
+        </form>
+      </div>
+    );
 }
