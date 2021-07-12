97ab55fe8a5dfd39175be1ae49048aa8b37b4749
ddrdusk <83031647+ddrdusk@users.noreply.github.com>
2021-07-12 19:33:25 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends React.Component
             onChange={this.handleChange}
             placeholder="Password"
           />
+          <button /*onClick={() => this.handleClick(tarea.id)}*/>Log in</button>
         </header>
       </div>
     );
